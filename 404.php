f77cf122902da0fa348abba3fea5bf65ff2bf4a2
Daniel Kaltenbaugh <daniel@dijatek.com>
2015-05-20 18:12:03 -0500

    Initial commit for local repository

---
+++ 404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Oops - that page wasn't found</p>
+    <p>Maybe you will have better luck on the <a href='index.php'>home page</a>.
+    </p>
+  </body>
+</html>
