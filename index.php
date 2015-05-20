aaa0526bdc05585cc454a0b1a83bb66063c71163
mitch <mitch@dijatek.com>
2015-05-20 12:55:53 -0600

    Pushing all files for first time.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+	</head>
+	<body>
+	<div id="container">
+		<div class="container-fluid main-container">
+			<div class="row" id="site-header">
+				<div class="col-sm-1">
+					<img src="images/logo.png" class="img-responsive" alt="Logo for Simple Website" />
+				</div>
+				<div class="col-sm-11">
+					<h1 class="" id="site-name">Simple Website</h1>
+				</div>
+			</div>
+		</div>
+		<nav class="navbar navbar-inverse nav-test">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+		<div id="body">
+			<div class="container-fluid main-container">
+				body stuff
+			</div>
+		</div>
+		<div class="container-fluid main-container">
+			<div class="row" id="site-footer">
+				<div class="col-sm-3">
+				</div>
+				<div class="col-sm-3">
+					<p class="text-center" style="color:white;">Footer text</p>
+				</div>
+				<div class="col-sm-3">
+				</div>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
